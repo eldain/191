8d3494599ca3f32df10fcd77a5e8c2b917adb84a
Daniel McInnis <danmcinnis@cox.net>
2016-11-12 11:02:56 -0800

    wip adding MDL dashboard template to laravel

---
+++ resources/views/base.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>@yield('title')</title>
+	<!-- <link rel="stylesheet" href="/css/app.css" media="screen"> -->
+	<link rel="stylesheet" href="/css/material.min.css" media="screen" title="no title">
+	<script src="/js/material.min.js" charset="utf-8"></script>
+	@yield('head')
+</head>
+<body>
+	@yield('body')
+</body>
+</html>
