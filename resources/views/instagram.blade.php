6f62e413ed9d7efe0b25a4f0f03ddff740a53074
lukeraus <lraus@uci.edu>
2016-11-28 14:22:45 -0800

    Instagram Login complete

--- resources/views/instagram.blade.php
+++ resources/views/instagram.blade.php
@@ -6,7 +6,28 @@
 
 <main class="mdl-layout__content mdl-color--grey-100">
 	<div class="mdl-grid demo-content">
-		<h3>Instagram Info goes on this Page</h3>
+		<h3>Instagram
+		<?php
+		use MetzWeb\Instagram\Instagram;
+
+        $instagram = new Instagram(array(
+            'apiKey'      => 'feac92c4402246d1b3aa77001c72e574',
+            'apiSecret'   => 'd87070d9f2aa432e845434c908d367dd',
+            'apiCallback' => 'https://rtdiprod.herokuapp.com/instagram'
+        ));
+
+        echo "<a href='{$instagram->getLoginUrl()}'>Login with Instagram</a>";
+
+        // grab OAuth callback code
+        if (isset($_GET['code'])){
+            $code = $_GET['code'];
+            $data = $instagram->getOAuthToken($code);
+
+            echo '<br>Your username is: ' . $data->user->username;
+        }
+
+        ?>
+		</h3>
 	</div>
 </main>
 
