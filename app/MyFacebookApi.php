fadfcc5d65c6ece4f75eb28c4e1567b015b159c7
lukeraus <lraus@uci.edu>
2017-01-19 17:39:03 -0800

    facebook reactions

--- app/MyFacebookApi.php
+++ app/MyFacebookApi.php
@@ -9,8 +9,12 @@ class MyFacebookApi
     private $appsecret = 'e336cc48fe592916c5968024714d7a89';
     private $FbGraphHost = 'https://graph.facebook.com/';
 
+
     /**
      * Get the number of likes on a Facebook page given the page ID.
+     *
+     * URL Called: https://graph.facebook.com/{$pageId}/?fields=fan_count&
+     * access_token={$appid}|{$appsecret}
      *
      * @return String
      */
@@ -27,6 +31,26 @@ class MyFacebookApi
         }else{
             return 0;
         }
+
+    }
+
+    /**
+     * Get a list of reaction per post in order of most recent.
+     *
+     * URL Called: https://graph.facebook.com/{$pageId}/
+     * feed/?fields=reactions&limit=2&
+     * access_token={$appid}|{$appsecret}
+     *
+     * @return String
+     */
+    public function fbRecentPost($pageId, $limit)
+    {
+        //Construct a Facebook URL
+        $json_url = $this->FbGraphHost . $pageId . '/feed/?fields=reactions&limit=' . $limit . '&access_token=' . $this->appid.'|'.$this->appsecret;
+        $json = file_get_contents($json_url);
+        //$json_output = json_decode($json);
+
+        return $json;
 
     }
 
--- resources/views/facebook.blade.php
+++ resources/views/facebook.blade.php
@@ -10,7 +10,9 @@
 			use App\MyFacebookApi;
 			$fb = new MyFacebookApi();
 			echo '<h3>GigaSavvy number of likes = ' . $fb->fbPageLikeCount('GigaSavvy') . '</h3>';
+		    echo '<code>' . $fb->fbRecentPost('GigaSavvy', 2) . '</code>';
 		?>
+		
 	</div>
 </main>
 
