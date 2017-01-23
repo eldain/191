d901c944c7cb10d991878777bc685869dd51fa99
Daniel McInnis <dmcinnis@uci.edu>
2017-01-23 12:16:53 -0800

    Mock line graph for Facebook page using SVG

--- resources/views/facebook.blade.php
+++ resources/views/facebook.blade.php
@@ -14,7 +14,49 @@
 		    echo '<h3> Number of Comments per post: ' . $fb->getNumberOfCommentsPerPost('GigaSavvy', 5) . '</h3>';
 		?>
 
+		<svg viewBox="0 0 500 100" class="chart">
+		  <polyline
+		     fill="none"
+		     stroke="#0074d9"
+		     stroke-width="2"
+		     points="
+		       00,120
+		       20,60
+		       40,80
+		       60,20
+		       80,80
+		       100,80
+		       120,60
+		       140,100
+		       160,90
+		       180,80
+		       200, 110
+		       220, 10
+		       240, 70
+		       260, 100
+		       280, 100
+		       300, 40
+		       320, 0
+		       340, 100
+		       360, 100
+		       380, 120
+		       400, 60
+		       420, 70
+		       440, 80
+		     "
+		   />
+		</svg>
+
 	</div>
+
+	<style media="screen">
+	.chart {
+		width: 500px;
+		height: 100px;
+		padding: 20px 20px;
+		border: 1px solid gold;
+	}
+	</style>
 </main>
 
 @stop
--- resources/views/settings.blade.php
+++ resources/views/settings.blade.php
@@ -1,42 +1,41 @@
-@extends('dashboard-template') @section('title', 'Settings') @section('dashboard-title', 'Settings') @section('dashboard-body')
+@extends('dashboard-template')
+@section('title', 'Settings')
+@section('dashboard-title', 'Settings')
+@section('dashboard-body')
 
 <main class="mdl-layout__content content-background">
 	<div class="mdl-grid demo-content">
 		<form action="#">
 			<h2 class="white">General</h2>
 			<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-				<input class="mdl-textfield__input" type="text" id="sample3">
-				<label class="mdl-textfield__label white" for="sample3">Name</label>
+				<input class="mdl-textfield__input" type="text" id="name">
+				<label class="mdl-textfield__label white" for="name">Name</label>
 			</div>
 			<br>
 			<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-				<input class="mdl-textfield__input" type="text" id="sample3">
-				<label class="mdl-textfield__label white" for="sample3">Password</label>
+				<input class="mdl-textfield__input" type="text" id="password">
+				<label class="mdl-textfield__label white" for="password">Password</label>
 			</div>
 			<br>
 			<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-				<input class="mdl-textfield__input" type="text" id="sample3">
-				<label class="mdl-textfield__label white" for="sample3">Email</label>
+				<input class="mdl-textfield__input" type="text" id="email">
+				<label class="mdl-textfield__label white" for="email">Email</label>
 			</div>
-		</form>
-	</div>
 
-	<div class="mdl-grid demo-content">
-		<form action="#">
 			<h2 class="white">API Credentials</h2>
 			<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-				<input class="mdl-textfield__input" type="text" id="sample3">
-				<label class="mdl-textfield__label white" for="sample3">Facebook</label>
+				<input class="mdl-textfield__input" type="text" id="facebook">
+				<label class="mdl-textfield__label white" for="facebook">Facebook</label>
 			</div>
 			<br>
 			<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-				<input class="mdl-textfield__input" type="text" id="sample3">
-				<label class="mdl-textfield__label white" for="sample3">Instagram</label>
+				<input class="mdl-textfield__input" type="text" id="instagram">
+				<label class="mdl-textfield__label white" for="instagram">Instagram</label>
 			</div>
 			<br>
 			<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-				<input class="mdl-textfield__input" type="text" id="sample3">
-				<label class="mdl-textfield__label white" for="sample3">Twitter</label>
+				<input class="mdl-textfield__input" type="text" id="twitter">
+				<label class="mdl-textfield__label white" for="twitter">Twitter</label>
 			</div>
 		</form>
 	</div>
