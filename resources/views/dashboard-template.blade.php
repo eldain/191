df1fb8dfc5674296cb83e7c5941459b8ab2e32ca
Daniel McInnis <dmcinnis@uci.edu>
2017-01-23 11:14:06 -0800

    Settings page rough draft

--- resources/views/dashboard-template.blade.php
+++ resources/views/dashboard-template.blade.php
@@ -2,6 +2,7 @@
 @section('title', 'Observation Deck')
 @section('head')
 	<link rel="stylesheet" href="/css/styles.css" media="screen" title="no title">
+	<link rel="stylesheet" href="/css/tachyons.min.css" media="screen" title="no title">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0">
 	<!-- <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:regular,bold,italic,thin,light,bolditalic,black,medium&amp;lang=en"> -->
 	<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
--- resources/views/settings.blade.php
+++ resources/views/settings.blade.php
@@ -1,13 +1,50 @@
-@extends('dashboard-template')
-@section('title', 'Settings')
-
-@section('dashboard-title', 'Settings')
-@section('dashboard-body')
+@extends('dashboard-template') @section('title', 'Settings') @section('dashboard-title', 'Settings') @section('dashboard-body')
 
 <main class="mdl-layout__content content-background">
 	<div class="mdl-grid demo-content">
-		<h1>This is the settings page</h1>
+		<form action="#">
+			<h2 class="white">General</h2>
+			<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+				<input class="mdl-textfield__input" type="text" id="sample3">
+				<label class="mdl-textfield__label white" for="sample3">Name</label>
+			</div>
+			<br>
+			<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+				<input class="mdl-textfield__input" type="text" id="sample3">
+				<label class="mdl-textfield__label white" for="sample3">Password</label>
+			</div>
+			<br>
+			<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+				<input class="mdl-textfield__input" type="text" id="sample3">
+				<label class="mdl-textfield__label white" for="sample3">Email</label>
+			</div>
+		</form>
 	</div>
+
+	<div class="mdl-grid demo-content">
+		<form action="#">
+			<h2 class="white">API Credentials</h2>
+			<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+				<input class="mdl-textfield__input" type="text" id="sample3">
+				<label class="mdl-textfield__label white" for="sample3">Facebook</label>
+			</div>
+			<br>
+			<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+				<input class="mdl-textfield__input" type="text" id="sample3">
+				<label class="mdl-textfield__label white" for="sample3">Instagram</label>
+			</div>
+			<br>
+			<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+				<input class="mdl-textfield__input" type="text" id="sample3">
+				<label class="mdl-textfield__label white" for="sample3">Twitter</label>
+			</div>
+		</form>
+	</div>
+
+	<div class="mdl-grid demo-content">
+		<button type="button" name="button" class="mdl-button white">Submit</button>
+	</div>
+
 </main>
 
 @stop
