8a0b123c8c90910e157cf427c70ee7c654546b52
Daniel McInnis <dmcinnis@uci.edu>
2017-01-26 14:19:18 -0800

    test javascript file for fetching FB info

---
+++ public/js/script.js
@@ -0,0 +1,17 @@
+function test(){
+  fetch('/fbReactionsPerPost?post_count=10')
+    .then((resp) => console.log(resp))
+    .catch(function(error) {
+      console.log('There has been a problem with your fetch operation: ' + error.message);
+    });
+}
+
+
+// this one works, just open up the promise in the console and you'll see a zen saying. when you open up the test() promise from above, says you need to login again
+function testOutside(){
+  fetch('https://api.github.com/zen')
+    .then((resp) => console.log(resp.text()))
+    .catch(function(error) {
+      console.log('There has been a problem with your fetch operation: ' + error.message);
+    });
+}
