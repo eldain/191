08b69d9c3904e5f2e314c5314d2e417f63abef1d
lukeraus <lraus@uci.edu>
2017-01-30 12:01:15 -0800

    Facebook get feed data updated

--- app/MyFacebookApi.php
+++ app/MyFacebookApi.php
@@ -37,6 +37,44 @@ class MyFacebookApi
     }
 
     /**
+     * Get a number of reactions and comments per post, with post times
+     * 
+     * URL Called: https://graph.facebook.com/{$pageId}/
+     * feed/?fields=message,updated_time,reactions.summary(total_count),
+     * comments.summary(total_count)&limit={$limit}&access_token={$appid}|{$appsecret}
+     *
+     * @return String in JSON format
+     */
+    public function getFeedData($pageId, $limit)
+    {
+        //Construct a Facebook URL
+        $json_url = $this->FbGraphHost . $pageId 
+        . '/feed/?fields=message,updated_time,reactions.summary(total_count),comments.summary(total_count)&limit='
+        . $limit . '&access_token=' . $this->appid.'|'.$this->appsecret;
+
+        $json = file_get_contents($json_url);
+        //TODO: Trim JSON
+        $json_output = json_decode($json);
+        $data_array = [];
+        if($json_output->data){
+
+            foreach ($json_output->data as $post){
+                $data_to_add = new \stdClass();
+                $data_to_add->message = $post->message;
+                $data_to_add->time = $post->updated_time;
+                $data_to_add->reactions = $post->reactions->summary->total_count;
+                $data_to_add->comments = $post->comments->summary->total_count;
+                array_push($data_array, $data_to_add);
+            }
+
+        }
+
+
+        return json_encode($data_array);
+
+    }
+
+    /**
      * Get a list of reaction per post in order of most recent.
      *
      * URL Called: https://graph.facebook.com/{$pageId}/
