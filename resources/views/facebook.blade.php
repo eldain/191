fdb421eebe2c9772a2a9c2b15fc674671473a078
Daniel McInnis <dmcinnis@uci.edu>
2017-02-13 12:06:10 -0800

    all charts on FB load on pageload

--- resources/views/facebook.blade.php
+++ resources/views/facebook.blade.php
@@ -8,18 +8,17 @@
 @section('dashboard-title', 'Facebook Updates')
 @section('dashboard-body')
 
-<main class="mdl-layout__content content-background flex flex-column flex-wrap vh-75">
-	<div class="main-chart flex justify-center items-center flex-auto bg-gray shadow-2 br2 black ma3 w-75 h-100 pointer">
-		<h1>CLICK HERE...</h1>
+<main class="mdl-layout__content content-background flex flex-column flex-wrap vh-100">
+	<div class="main-chart flex justify-center items-center flex-auto bg-gray shadow-2 br2 black ma3 w-70 h-100">
 	</div>
 
-	<div class="sub-chart-one flex-auto bg-gray shadow-2 br2 black ma3 w-75">
+	<div class="sub-chart-one flex-auto bg-gray shadow-2 br2 black ma3 w-25 h1">
 	</div>
 
-	<div class="sub-chart-two flex-auto bg-gray shadow-2 br2 black ma3 w-75">
+	<div class="sub-chart-two flex-auto bg-gray shadow-2 br2 black ma3 w-25 h1">
 	</div>
 
-	<div class="sub-chart-three flex-auto bg-gray shadow-2 br2 black ma3 w-75">
+	<div class="sub-chart-three flex-auto bg-gray shadow-2 br2 black ma3 w-25 h1">
 	</div>
 </main>
 
