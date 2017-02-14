2f2894cdd74233c49d63292e1b9dd7be1f6fe0d8
Daniel McInnis <dmcinnis@uci.edu>
2017-02-13 18:48:29 -0800

    quic fix: change FB card background color to the same color as chart background

--- resources/views/facebook.blade.php
+++ resources/views/facebook.blade.php
@@ -9,16 +9,16 @@
 @section('dashboard-body')
 
 <main class="mdl-layout__content content-background flex flex-column flex-wrap vh-100">
-	<div class="main-chart flex justify-center items-center flex-auto bg-gray shadow-2 br2 black ma3 w-70 h-100">
+	<div class="main-chart flex justify-center items-center flex-auto bg-darker-grey shadow-2 br2 black ma3 w-70 h-100">
 	</div>
 
-	<div class="sub-chart-one flex-auto bg-gray shadow-2 br2 black ma3 w-25 h1">
+	<div class="sub-chart-one flex-auto bg-darker-grey shadow-2 br2 black ma3 w-25 h1">
 	</div>
 
-	<div class="sub-chart-two flex-auto bg-gray shadow-2 br2 black ma3 w-25 h1">
+	<div class="sub-chart-two flex-auto bg-darker-grey shadow-2 br2 black ma3 w-25 h1">
 	</div>
 
-	<div class="sub-chart-three flex-auto bg-gray shadow-2 br2 black ma3 w-25 h1">
+	<div class="sub-chart-three flex-auto bg-darker-grey shadow-2 br2 black ma3 w-25 h1">
 	</div>
 </main>
 
