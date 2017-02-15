26214c7c65438b4bd8fe46fb904f59978620c01c
Daniel McInnis <dmcinnis@uci.edu>
2017-02-14 23:11:13 -0800

    Dummy data used on dashboard cards for presentation with GS

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,16 +4,23 @@
 @section('dashboard-body')
 <main class="mdl-layout__content content-background flex">
 	<div class="card bg-darker-grey w3 h5 flex-auto ma4 br2 shadow-2">
-		<h1 class="white ml3">Facebook</h1>
-		<a class="link white ml3" href="/facebook">Link</a>
+		<div class="white ma3 f2">Facebook</div>
+		<div class="white f3 ma3 mt4"><i class="material-icons txt-gold pr3">favorite</i>45 Reactions</div>
+		<div class="white f3 ma3"><i class="material-icons txt-gold pr3">comment</i>78 Comments</div>
+		<div class="white f3 ma3"><i class="material-icons txt-gold pr3">visibility</i>270 Views</div>
+		<a class="link txt-gold ma3 pointer" href="/facebook">Across 16 posts</a>
 	</div>
-	<div class="card bg-darker-grey w3 h5 flex-auto ma4 br2 shadow-2">
-		<h1 class="white ml3">Twitter</h1>
-		<a class="link white ml3" href="/twitter">Link</a>
+	<div class="card bg-darker-grey w3 h5 flex-auto ma4 br2 shadow-2 flex flex-column content-around">
+		<div class="white ma3 f2">Twitter</div>
+		<div class="white f3 ma3"><i class="material-icons txt-gold pr3">favorite</i>45 Favorites</div>
+		<div class="white f3 ma3"><i class="material-icons txt-gold pr3">comment</i>78 Retweets</div>
+		<a class="link txt-gold ma3 pointer" href="/twitter">Across 16 tweets</a>
 	</div>
-	<div class="card bg-darker-grey w3 h5 flex-auto ma4 br2 shadow-2">
-		<h1 class="white ml3">Instagram</h1>
-		<a class="link white ml3" href="/instagram">Link</a>
+	<div class="card bg-darker-grey w3 h5 flex-auto ma4 br2 shadow-2 flex flex-column content-around">
+		<div class="white ma3 f2">Instagram</div>
+		<div class="white f3 ma3"><i class="material-icons txt-gold pr3">favorite</i>45 Likes</div>
+		<div class="white f3 ma3"><i class="material-icons txt-gold pr3">comment</i>78 Comments</div>
+		<a class="link txt-gold ma3 pointer" href="/instagram">Across 16 posts</a>
 	</div>
 </main>
 
