e1234af03e81d60256c16a0fdc9e652400ddf0dc
Daniel McInnis <dmcinnis@uci.edu>
2017-02-16 16:21:47 -0800

    remove async from script src tags

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -6,7 +6,7 @@
 	<!-- <link rel="stylesheet" href="/css/app.css" media="screen"> -->
 	<link rel="stylesheet" href="/css/material.min.css" media="screen" title="no title">
 	<script src="/js/material.min.js" charset="utf-8"></script>
-	<script src="https://www.gstatic.com/charts/loader.js" type="text/javascript" async></script>
+	<script src="https://www.gstatic.com/charts/loader.js" type="text/javascript"></script>
 	@yield('head')
 	@yield('scripts')
 </head>
