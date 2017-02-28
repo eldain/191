8bb0ff159a490ef0de22f362cb2b180e0b87c43e
Daniel McInnis <dmcinnis@uci.edu>
2017-02-27 16:12:16 -0800

    WIP Loading spinner for dashboard cards

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,11 +8,19 @@
 @section('dashboard-body')
 <main class="mdl-layout__content content-background flex">
 	<div class="fb-card card bg-darker-grey w3 h5 flex-auto ma4 br2 shadow-2 pointer hover-bg-dark-gray">
+
+		<div class="w-100 h-100 bg-near-black z-1 flex justify-center items-center dn">
+			<svg class="spinner" width="65px" height="65px" viewBox="0 0 66 66" xmlns="http://www.w3.org/2000/svg">
+			   <circle class="path" fill="none" stroke="#AB9657" stroke-width="6" stroke-linecap="round" cx="33" cy="33" r="30"></circle>
+			</svg>
+		</div>
+
 		<div class="white ma3 f2">Facebook <span class="f3">({{ Auth::user()->facebook }})</span></div>
 		<div class="white f3 ma3 mt4"><i class="material-icons txt-gold pr3">thumb_up</i><span id="fb-views">-</span> total page likes</div>
 		<div class="white f3 ma3"><i class="material-icons txt-gold pr3">favorite</i><span id="fb-reactions">-</span> reactions</div>
 		<div class="white f3 ma3"><i class="material-icons txt-gold pr3">comment</i><span id="fb-comments">-</span> comments</div>
 		<div class="txt-gold ma3">Across <span id="fb-posts">-</span> posts in <span id="fb-time-frame">-</span> days</div>
+
 	</div>
 	<div class="card bg-darker-grey w3 h5 flex-auto ma4 br2 shadow-2 flex flex-column content-around">
 		<div class="white ma3 f2">Twitter <span class="f3">({{ Auth::user()->twitter }})</span></div>
