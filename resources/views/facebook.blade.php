5b22d25b655522bffb22f3a4fe6166395355609f
Daniel McInnis <dmcinnis@uci.edu>
2017-03-01 11:57:57 -0800

    favicon and button indicator for date range

--- resources/views/facebook.blade.php
+++ resources/views/facebook.blade.php
@@ -12,7 +12,7 @@ Facebook Updates ({{ Auth::user()->facebook }})
 
 <main class="mdl-layout__content content-background flex flex-column flex-wrap vh-100">
 	<div class="button-holder flex z-1 absolute w-30 justify-between">
-		<button class="flex-auto f5 mw4 pv1 bn br2 bg-black white" data-days="30" type="button" name="30day">30 Day</button>
+		<button class="flex-auto f5 mw4 pv1 bn br2 bg-black white" data-days="30" type="button" name="30day">30 Days</button>
 		<button class="flex-auto f5 mw4 pv1 bn br2 bg-black white" data-days="60" type="button" name="60days">60 Days</button>
 		<button class="flex-auto f5 mw4 pv1 bn br2 bg-black white" data-days="90" type="button" name="90days">90 Days</button>
 	</div>
