0df3d752fb6bb24576826c044e4813a415fa25a2
lukeraus <lraus@uci.edu>
2017-03-01 12:09:16 -0800

    added twitter graphs (draft)

---
+++ public/js/twitter-script.js
@@ -0,0 +1,285 @@
+// Remember that userFB is set on dashboard page
+
+const mainChart = document.querySelector('.main-chart');
+const firstSubChart = document.querySelector('.sub-chart-one');
+const secondSubChart = document.querySelector('.sub-chart-two');
+const thirdSubChart = document.querySelector('.sub-chart-three');
+const dayButtons = document.querySelectorAll(".button-holder button");
+
+/*
+- get data
+- filter data for specific chart
+- pass filtered data to google chart which...
+- update/render panels with charts
+*/
+
+function getTodaysDate(){
+  var today = new Date();
+  var dd = today.getDate();
+  var mm = today.getMonth()+1; //January is 0!
+  var yyyy = today.getFullYear();
+
+  if(dd<10) {
+      dd='0'+dd
+  }
+
+  if(mm<10) {
+      mm='0'+mm
+  }
+
+  today = mm+'/'+dd+'/'+yyyy;
+  return today;
+}
+
+function getStartDate(daysPassed){
+  var today = new Date();
+  today.setDate(today.getDate() - daysPassed);
+  var dd = today.getDate();
+  var mm = today.getMonth()+1; //January is 0!
+  var yyyy = today.getFullYear();
+
+  if(dd<10) {
+      dd='0'+dd
+  }
+
+  if(mm<10) {
+      mm='0'+mm
+  }
+
+  today = mm+'/'+dd+'/'+yyyy;
+  return today;
+}
+
+function getData(start, end){
+  let myURL = `/twGetTweets?user=${userFB}&since=${start}&until=${end}`
+  return fetch(myURL)
+    .then(resp => {
+      if(resp.ok){
+        return resp.json();
+      }
+      throw new Error('Network response was not ok.');
+    })
+    .then(value => {
+      let allData = filterForAll(value);
+      let favorites = filterForFavorites(value);
+      let retweets = filterForRetweets(value);
+      drawMainChart(allData);
+      drawSubChartOne(favorites);
+      drawSubChartTwo(retweets);
+    })
+    .catch(function(error) {
+      console.log('There has been a problem with your fetch operation: ' + error.message);
+    });
+}
+
+function filterForRetweets(data){
+  let retweetArray = data.map(post => {
+    let date = new Date(post.created_at);
+    let month = date.getMonth() + 1;
+    let day = date.getDate();
+    return [`${month}/${day}`, post.retweet_count]
+  });
+  return retweetArray.reverse();
+}
+
+function filterForFavorites(data){
+  let favoritesArray = data.map(post => {
+    let date = new Date(post.created_at);
+    let month = date.getMonth() + 1;
+    let day = date.getDate();
+    return [`${month}/${day}`, post.favorite_count]
+  });
+  return favoritesArray.reverse();
+}
+
+function filterForAll(data){
+  let allArray = data.map(post => {
+    let date = new Date(post.created_at);
+    let month = date.getMonth() + 1;
+    let day = date.getDate();
+    return [`${month}/${day}`, post.favorite_count, post.retweet_count]
+  });
+  return allArray.reverse();
+}
+
+google.charts.load('current', {packages: ['corechart', 'line']});
+google.charts.setOnLoadCallback(() => {
+  let startDate = getStartDate(10);
+  let endDate = getTodaysDate();
+  getData(startDate, endDate, "favorites");
+});
+
+function drawMainChart(chartData) {
+      var data = new google.visualization.DataTable();
+      data.addColumn('string', 'Posts');
+      data.addColumn('number', 'Favorites');
+      data.addColumn('number', 'Retweet');
+
+      // addRows format [[a,b],[c,d],...]
+      data.addRows(chartData);
+
+      var options = {
+        'title': "Overall Twitter Trends ",
+        titleTextStyle: {
+          color: '#fff',
+          fontSize: 20
+        },
+        legendTextStyle: { color: '#FFF' },
+        hAxis: {
+          title: 'Post Date',
+          baselineColor: '#FFF',
+          gridlineColor: '#FFF',
+          textStyle:{color: '#FFF'},
+          titleTextStyle: {color: '#FFF'}
+        },
+        vAxis: {
+          title: 'Trends',
+          baselineColor: '#FFF',
+          gridlineColor: '#FFF',
+          textStyle:{color: '#FFF'},
+          titleTextStyle: {color: '#FFF'}
+        },
+        //need to figure out ability to resize
+        width: mainChart.innerWidth,
+        height: mainChart.innerHeight,
+        colors: ['#BBA43F',"#57A773", "#AFA2FF"],
+        backgroundColor: {
+          fill: '#404040',
+        }
+      };
+
+      var chart = new google.visualization.LineChart(mainChart);
+      chart.draw(data, options);
+}
+
+function drawSubChartOne(chartData) {
+  var data = new google.visualization.DataTable();
+  data.addColumn('string', 'Posts');
+  data.addColumn('number', 'Likes');
+
+  // addRows format [[a,b],[c,d],...]
+  data.addRows(chartData);
+
+  var options = {
+    'title': "Favorites",
+    titleTextStyle: {
+      color: '#fff',
+      fontSize: 16
+    },
+    legendTextStyle: { color: '#FFF' },
+    hAxis: {
+      title: 'Posts',
+      baselineColor: '#FFF',
+      gridlineColor: '#FFF',
+      textStyle:{color: '#FFF'},
+      titleTextStyle: {color: '#FFF'}
+    },
+    vAxis: {
+      title: 'Retweets',
+      baselineColor: '#FFF',
+      gridlineColor: '#FFF',
+      textStyle:{color: '#FFF'},
+      titleTextStyle: {color: '#FFF'}
+    },
+    //need to figure out ability to resize
+    width: firstSubChart.innerWidth,
+    height: firstSubChart.innerHeight,
+    colors: ['#BBA43F'],
+    backgroundColor: {
+      fill: '#404040',
+    }
+  };
+
+  var chart = new google.visualization.LineChart(firstSubChart);
+  chart.draw(data, options);
+}
+
+function drawSubChartTwo(chartData) {
+  var data = new google.visualization.DataTable();
+  data.addColumn('string', 'Posts');
+  data.addColumn('number', 'Likes');
+
+  // addRows format [[a,b],[c,d],...]
+  data.addRows(chartData);
+
+  var options = {
+    'title': "Retweets",
+    titleTextStyle: {
+      color: '#fff',
+      fontSize: 16
+    },
+    legendTextStyle: { color: '#FFF' },
+    hAxis: {
+      title: 'Posts',
+      baselineColor: '#FFF',
+      gridlineColor: '#FFF',
+      textStyle:{color: '#FFF'},
+      titleTextStyle: {color: '#FFF'}
+    },
+    vAxis: {
+      title: 'Favorites',
+      baselineColor: '#FFF',
+      gridlineColor: '#FFF',
+      textStyle:{color: '#FFF'},
+      titleTextStyle: {color: '#FFF'}
+    },
+    //need to figure out ability to resize
+    width: secondSubChart.innerWidth,
+    height: secondSubChart.innerHeight,
+    colors: ["#57A773"],
+    backgroundColor: {
+      fill: '#404040',
+    }
+  };
+
+  var chart = new google.visualization.LineChart(secondSubChart);
+  chart.draw(data, options);
+}
+
+function drawSubChartThree(chartData) {
+  var data = new google.visualization.DataTable();
+  data.addColumn('string', 'Posts');
+  data.addColumn('number', 'Likes');
+
+  // addRows format [[a,b],[c,d],...]
+  data.addRows(chartData);
+
+  var options = {
+    'title': "Followers",
+    titleTextStyle: {
+      color: '#fff',
+      fontSize: 16
+    },
+    legendTextStyle: { color: '#FFF' },
+    hAxis: {
+      title: 'Posts',
+      baselineColor: '#FFF',
+      gridlineColor: '#FFF',
+      textStyle:{color: '#FFF'},
+      titleTextStyle: {color: '#FFF'}
+    },
+    vAxis: {
+      title: 'Shares',
+      baselineColor: '#FFF',
+      gridlineColor: '#FFF',
+      textStyle:{color: '#FFF'},
+      titleTextStyle: {color: '#FFF'}
+    },
+    //need to figure out ability to resize
+    width: thirdSubChart.innerWidth,
+    height: thirdSubChart.innerHeight,
+    colors: ["#AFA2FF"],
+    backgroundColor: {
+      fill: '#404040',
+    }
+  };
+
+  var chart = new google.visualization.LineChart(thirdSubChart);
+  chart.draw(data, options);
+}
+
+dayButtons.forEach(button => button.addEventListener('click', (e) => {
+  let startDate = getStartDate(e.target.dataset.days);
+  let endDate = getTodaysDate();
+  getData(startDate, endDate);
+}));
