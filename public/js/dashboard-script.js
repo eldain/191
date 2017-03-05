feb608635e828eccaaf84caf1f8e7f4271f65935
Daniel McInnis <dmcinnis@uci.edu>
2017-03-05 09:58:24 -0800

    Instagram dashboard card

--- public/js/dashboard-script.js
+++ public/js/dashboard-script.js
@@ -13,10 +13,20 @@ const twitterRetweets = document.querySelector('#twitter-retweets');
 const twitterTweets = document.querySelector('#twitter-tweets');
 const twitterTimeFrame = document.querySelector('#twitter-time-frame');
 
+const igCard = document.querySelector(".ig-card");
+const igFollowers = document.querySelector('#ig-followers');
+const igLikes = document.querySelector('#ig-likes');
+const igComments = document.querySelector('#ig-comments');
+const igPosts = document.querySelector('#ig-posts');
+const igTimeFrame = document.querySelector('#ig-time-frame');
+
 fbCard.addEventListener("click", () => {
   window.location.href = '/facebook';
 })
 twitterCard.addEventListener("click", () => {
+  window.location.href = '/twitter';
+})
+igCard.addEventListener("click", () => {
   window.location.href = '/twitter';
 })
 
@@ -90,6 +100,19 @@ function endTwitterLoader(){
   twitterLoader.classList.remove('flex');
   twitterLoader.classList.add('dn');
 }
+function startIGLoader(){
+  var igLoader = document.querySelector('.ig-loader');
+  var loaderWidth = igCard.clientWidth;
+  var loaderHeight = igCard.clientHeight;
+  igLoader.setAttribute('style', `height:${loaderHeight}px;width:${loaderWidth}px;`);
+  igLoader.classList.remove('dn');
+  igLoader.classList.add('flex');
+}
+function endIGLoader(){
+  var igLoader = document.querySelector('.ig-loader');
+  igLoader.classList.remove('flex');
+  igLoader.classList.add('dn');
+}
 
 // Facebook specific data manipulation functions
 function getFBTotalReactions(data){
@@ -137,6 +160,30 @@ function getTwitterRetweets(data){
 function getTwitterTotalTweets(data){
 	var totalTweets = data.length;
 	return totalTweets;
+}
+
+// Instagram specific data manipulation functions
+function getIGLikes(data){
+	var likes = data.map(post => {
+		return post.likes;
+	})
+	.reduce(function(acc, val) {
+		return acc + val;
+	}, 0);
+	return likes;
+}
+function getIGComments(data){
+	var totalComments = data.map(post => {
+		return post.comments_count;
+	})
+	.reduce(function(acc, val) {
+		return acc + val;
+	}, 0);
+	return totalComments;
+}
+function getIGTotalPosts(data){
+	var totalPosts = data.length;
+	return totalPosts;
 }
 
 // Get the Facebook data
@@ -223,16 +270,66 @@ function getTwitterFollowers(){
     });
 }
 
+// Get the Instagram Data
+function getIGData(end){
+  startIGLoader();
+  let myURL = `/inGetRecentPosts?user=${userInstagram}&until=${end}`;
+  return fetch(myURL)
+    .then(resp => {
+      if(resp.ok){
+        return resp.json();
+      }
+      throw new Error('Network response was not ok.');
+    })
+    .then(value => {
+      var igTotalLikes = getIGLikes(value);
+      var igTotalComments = getIGComments(value);
+      var igTotalPosts = getIGTotalPosts(value);
+      igLikes.innerHTML = numberWithCommas(igTotalLikes);
+      igComments.innerHTML = numberWithCommas(igTotalComments);
+      igPosts.innerHTML = numberWithCommas(igTotalPosts);
+      igTimeFrame.innerHTML = dayCount;
+      endIGLoader();
+    })
+    .catch(function(error) {
+      console.log('There has been a problem with your fetch operation: ' + error.message);
+    });
+}
+function getIGFollowers(){
+  let myURL = `/inGetNumberOfFollowers?user=${userInstagram}`;
+  fetch(myURL)
+    .then(resp => {
+      if(resp.ok){
+        return resp.json();
+      }
+      throw new Error('Network response was not ok.');
+    })
+    .then(value => {
+      igFollowers.innerHTML = numberWithCommas(value);
+    })
+    .catch(function(error) {
+      console.log('There has been a problem with your fetch operation: ' + error.message);
+    });
+}
+
 // Actually call the functions
 getFBData(getStartDate(dayCount), getTodaysDate());
 getFBPageLikes();
 getTwitterData(getStartDate(dayCount));
 getTwitterFollowers();
+getIGData(getStartDate(dayCount));
+getIGFollowers();
 
 // Repeat for "Realtime Data"
 setInterval(() => {
-  getFBData(getStartDate(dayCount), getTodaysDate());
+  var startDate = getStartDate(dayCount);
+
+  getFBData(startDate, getTodaysDate());
   getFBPageLikes();
-  getTwitterData(getStartDate(dayCount));
+
+  getTwitterData(startDate);
   getTwitterFollowers();
+
+  getIGData(startDate);
+  getIGFollowers();
 }, 60000);
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -32,18 +32,26 @@
 		</div>
 
 		<div class="white ma3 f2">Twitter <span class="f3">({{ Auth::user()->twitter }})</span></div>
-		<div class="white f3 ma3 mt4"><i class="material-icons txt-gold pr3">thumb_up</i><span id="twitter-followers">-</span> total followers</div>
+		<div class="white f3 ma3 mt4"><i class="material-icons txt-gold pr3">group</i><span id="twitter-followers">-</span> total followers</div>
 		<div class="white f3 ma3"><i class="material-icons txt-gold pr3">favorite</i><span id="twitter-favorites">-</span> favorites</div>
-		<div class="white f3 ma3"><i class="material-icons txt-gold pr3">comment</i><span id="twitter-retweets">-</span> retweets</div>
+		<div class="white f3 ma3"><i class="material-icons txt-gold pr3">cached</i><span id="twitter-retweets">-</span> retweets</div>
 		<div class="txt-gold ma3">Across <span id="twitter-tweets">-</span> tweets in <span id="twitter-time-frame">-</span> days</div>
 
-
 	</div>
-	<div class="card bg-darker-grey w3 h5 flex-auto ma4 br2 shadow-2 flex flex-column content-around">
+	<div class="ig-card card bg-darker-grey w3 h5 flex-auto ma4 br2 shadow-2 pointer hover-bg-dark-gray">
+
+		<div class="ig-loader bg-near-black absolute z-1 flex justify-center items-center o-80">
+			<svg class="spinner" width="65px" height="65px" viewBox="0 0 66 66" xmlns="http://www.w3.org/2000/svg">
+				 <circle class="path" fill="none" stroke="#AB9657" stroke-width="6" stroke-linecap="round" cx="33" cy="33" r="30"></circle>
+			</svg>
+		</div>
+
 		<div class="white ma3 f2">Instagram <span class="f3">({{ Auth::user()->instagram }})</span></div>
-		<div class="white f3 ma3"><i class="material-icons txt-gold pr3">favorite</i>45 Likes</div>
-		<div class="white f3 ma3"><i class="material-icons txt-gold pr3">comment</i>78 Comments</div>
-		<a class="link txt-gold ma3 pointer" href="/instagram">Across 16 posts</a>
+		<div class="white f3 ma3 mt4"><i class="material-icons txt-gold pr3">group</i><span id="ig-followers">-</span> total followers</div>
+		<div class="white f3 ma3"><i class="material-icons txt-gold pr3">favorite</i><span id="ig-likes">-</span> likes</div>
+		<div class="white f3 ma3"><i class="material-icons txt-gold pr3">comment</i><span id="ig-comments">-</span> comments</div>
+		<div class="txt-gold ma3">Across <span id="ig-posts">-</span> posts in <span id="ig-time-frame">-</span> days</div>
+
 	</div>
 </main>
 @stop
